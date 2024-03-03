2c63d72fbed690a1dd33e2cc742ea19825dcfd15
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-03 17:45:38 -0500

    Conversión de cm a pulgadas

---
+++ conversiónDeUnidadesALongitudes.py
@@ -0,0 +1,7 @@
+# #Conversión de unidades de longitud
+
+# Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+
+cm=int(input("ingresa los cm que quieres transformar a pulgadas: "))
+pulgada= cm/2.54
+print("Tus ",cm,"cm, son",pulgada," pulgadas")
