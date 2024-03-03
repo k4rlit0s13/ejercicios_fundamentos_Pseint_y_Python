fd82dcc2a6d0b003c377ab7f81ec047bfef88f0f
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-03 17:33:54 -0500

    Promedio

---
+++ promedio.py
@@ -0,0 +1,10 @@
+# Promedio
+
+# Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+print("Hola usuario :D, por favor digita 4 valores para promediar ")
+dato1=int(input("Valor 1: "))
+dato2=int(input("Valor 2: "))
+dato3=int(input("Valor 3: "))
+dato4=int(input("Valor 4: "))
+promedio=(dato1+dato2+dato3+dato4)/4
+print("El promedio de tus dígitos es: ", promedio)
