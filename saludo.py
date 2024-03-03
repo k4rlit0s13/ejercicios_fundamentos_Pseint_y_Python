99bb58ce9eab67693db5b412c9d038cf92376070
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-03 17:00:00 -0500

    saludo

---
+++ saludo.py
@@ -0,0 +1,8 @@
+# Saludo
+
+# Escriba un programa que pida al usuario que escriba su 
+#nombre, y lo salude llamándolo por su nombre.
+
+nombre=input("Hola :D usuario, como te llamas? ")
+
+print("Es un gusto saludarte ",nombre)
