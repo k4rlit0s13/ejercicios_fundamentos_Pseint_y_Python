70ccbba9b93192fd97e811fcb7b7dd122655def5
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-13 19:19:12 -0500

    avance huevos a la copa

---
+++ huevosALaCopa.py
@@ -0,0 +1,12 @@
+hPequeño=47
+hGrande=67
+gxcm=1.038
+ccalórica=3.7
+conductividadTermica=5.4e-3
+tAguaEbullicion=100
+
+tOriginal=float(input("ingresa "))
+tDeseada=float(input(""))
+
+promedioMasaHuevo=(hPequeño+hGrande)/2
+
